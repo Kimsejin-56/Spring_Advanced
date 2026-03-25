82fc1c947c68b1353820c5bb6136cf1f3d4ef51f
Kimsejin-56 <saj5877@naver.com>
2026-03-25 13:48:18 +0900

    프록시 패턴 - 예제 코드1

--- proxy/src/main/java/hello/proxy/app/v3/OrderControllerV3.java
+++ proxy/src/main/java/hello/proxy/app/v3/OrderControllerV3.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping
 public class OrderControllerV3 {
     private final OrderServiceV3 orderService;
 
