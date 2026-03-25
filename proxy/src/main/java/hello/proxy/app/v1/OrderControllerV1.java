81bccabb0400eb33fff66cedd7bc7617b9a7b93b
Kimsejin-56 <saj5877@naver.com>
2026-03-25 12:57:34 +0900

    예제 프로젝트 만들기 v2

--- proxy/src/main/java/hello/proxy/app/v1/OrderControllerV1.java
+++ proxy/src/main/java/hello/proxy/app/v1/OrderControllerV1.java
@@ -1,12 +1,9 @@
 package hello.proxy.app.v1;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping //스프링은 @Controller 또는 @RequestMapping이 있어야 스프링 컨트롤러로 인식
-@ResponseBody
+@RestController
+@RequestMapping //스프링은 @Controller 또는 @RestController가 있어야 스프링 컨트롤러로 인식
 public interface OrderControllerV1 {
     @GetMapping("/v1/request")
     String request(@RequestParam("itemId") String itemId);
