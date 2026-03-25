32c7c9c7e42b68c1ac57e91e7732e9942e9cab67
Kimsejin-56 <saj5877@naver.com>
2026-03-25 14:21:47 +0900

    데코레이터 패턴 - 예제 코드2

--- proxy/src/test/java/hello/proxy/decorator/DecoratorPatternTest.java
+++ proxy/src/test/java/hello/proxy/decorator/DecoratorPatternTest.java
@@ -2,6 +2,7 @@ package hello.proxy.decorator;
 
 import hello.proxy.decorator.code.Component;
 import hello.proxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.decorator.code.MessageDecorator;
 import hello.proxy.decorator.code.RealComponent;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,14 @@ public class DecoratorPatternTest {
     void noDecorator() {
         Component realComponent = new RealComponent();
         DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+
+    @Test
+    void decorator1() {
+        RealComponent realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
         client.execute();
     }
 }
---
+++ proxy/src/test/java/hello/proxy/decorator/code/MessageDecorator.java
@@ -0,0 +1,24 @@
+package hello.proxy.decorator.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 데코레이터 패턴 - 새로운 기능 추가, 부가 기능 추가 주 목적
+ */
+@Slf4j
+public class MessageDecorator implements Component{
+    private Component component;
+
+    public MessageDecorator(Component component) {
+        this.component = component;
+    }
+
+    @Override
+    public String operation() {
+        log.info("MessageDecorator 실행");
+        String result = component.operation();
+        String decoResult = "***" + result + "***";
+        log.info("MessageDecorator 꾸미기 적용 전={}, 적용 후={}", result, decoResult);
+        return decoResult;
+    }
+}
