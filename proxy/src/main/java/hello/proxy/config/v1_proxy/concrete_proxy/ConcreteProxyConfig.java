71ee1f2408f45086ea29fc17940f74a8164f9a21
Kimsejin-56 <saj5877@naver.com>
2026-03-26 15:12:49 +0900

    구체 클래스 기반 프록시 - 적용

---
+++ proxy/src/main/java/hello/proxy/config/v1_proxy/concrete_proxy/ConcreteProxyConfig.java
@@ -0,0 +1,29 @@
+package hello.proxy.config.v1_proxy.concrete_proxy;
+
+import hello.proxy.app.v2.OrderControllerV2;
+import hello.proxy.app.v2.OrderRepositoryV2;
+import hello.proxy.app.v2.OrderServiceV2;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConcreteProxyConfig {
+    @Bean
+    public OrderControllerV2 orderController(LogTrace trace) {
+        OrderControllerV2 controllerImpl = new OrderControllerV2(orderService(trace));
+        return new OrderControllerConcreteProxy(controllerImpl, trace);
+    }
+
+    @Bean
+    public OrderServiceV2 orderService(LogTrace trace) {
+        OrderServiceV2 serviceImpl = new OrderServiceV2(orderRepository(trace));
+        return new OrderServiceConcreteProxy(serviceImpl, trace);
+    }
+
+    @Bean
+    public OrderRepositoryV2 orderRepository(LogTrace trace) {
+        OrderRepositoryV2 repositoryImpl = new OrderRepositoryV2();
+        return new OrderRepositoryConcreteProxy(repositoryImpl, trace);
+    }
+}
--- proxy/src/main/java/hello/proxy/config/v1_proxy/interface_proxy/InterfaceProxyConfig.java
+++ proxy/src/main/java/hello/proxy/config/v1_proxy/interface_proxy/InterfaceProxyConfig.java
@@ -1,9 +1,6 @@
-package hello.proxy.config.v1_proxy;
+package hello.proxy.config.v1_proxy.interface_proxy;
 
 import hello.proxy.app.v1.*;
-import hello.proxy.config.v1_proxy.interface_proxy.OrderControllerInterfaceProxy;
-import hello.proxy.config.v1_proxy.interface_proxy.OrderRepositoryInterfaceProxy;
-import hello.proxy.config.v1_proxy.interface_proxy.OrderServiceInterfaceProxy;
 import hello.proxy.trace.logtrace.LogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
