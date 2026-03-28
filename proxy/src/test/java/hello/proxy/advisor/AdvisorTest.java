8de4e4fba3e70dfc38680838091c0786b5d4e13c
Kimsejin-56 <saj5877@naver.com>
2026-03-28 17:18:18 +0900

    예제 코드2 - 직접 만든 포인트컷

--- proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
+++ proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -3,16 +3,23 @@ package hello.proxy.advisor;
 import hello.proxy.common.advice.TimeAdvice;
 import hello.proxy.common.service.ServiceImpl;
 import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+import java.lang.reflect.Method;
 
 /**
  * PointCut : 어디에 프록시를 적용할지 말지 필터링 역할
  * Advice : 프록시 로직, 생성된 프록시에 적용하는 부가 기능 로직
  * Advisor : 하나의 포인트 컷과 하나의 어드바이스를 가짐, 즉 어디에 어떤 로직을 적용할지 아는 것
  */
+@Slf4j
 public class AdvisorTest {
 
     @Test
@@ -26,5 +33,54 @@ public class AdvisorTest {
 
         proxy.save();
         proxy.find();
+    }
+
+    @Test
+    @DisplayName("직접 만든 포인트컷")
+    void advisorTest2() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        //프록시 팩토리 사용할 때 어드바이저 필수!!
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointCut(), new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    static class MyPointCut implements Pointcut {
+
+        @Override
+        public ClassFilter getClassFilter() {
+            return ClassFilter.TRUE;
+        }
+
+        @Override
+        public MethodMatcher getMethodMatcher() {
+            return new MyMethodMatcher();
+        }
+    }
+    
+    static class MyMethodMatcher implements MethodMatcher{
+        private String matchName = "save";
+        
+        @Override
+        public boolean matches(Method method, Class<?> targetClass) {
+            boolean result = method.getName().equals(matchName);
+            log.info("포인트컷 호출 method={}, targetClass={}", method.getName(), targetClass);
+            log.info("포인트컷 결과 result={}", result);
+            return result;
+        }
+
+        @Override
+        public boolean isRuntime() {
+            return false;
+        }
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass, Object... args) {
+            return false;
+        }
     }
 }
